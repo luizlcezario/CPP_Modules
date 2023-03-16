60d6f4e48d9dfca7a378d712eb657c9046e35667
luizlcezario <luizlcezario@gmail.com>
2023-03-16 01:27:31 -0300

    final_CPP7

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -2,10 +2,17 @@
 #include "iter.hpp"
 
 int main( void ) {
-	int a[9] = {50, 56, 80, 90, 103, 110, 120, 130, 14};
+	int a[9] = {50, 56, 83, 90, 103, 110, 120, 131, 14};
+	char b[9] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+	double c[9] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9};
 
+	std::cout << "Array of int" << std::endl;
 	::iter(a, 9, print);
 	::iter(a, 9, even);
-	::iter(a, 9, print);
+	std::cout << "Array of char" << std::endl;
+	::iter(b, 9, print);
+	::iter(b, 9, even);
+	std::cout << "Array of Float" << std::endl;
+	::iter(c, 9, print);
 	return 0;
 }
---
+++ CPP07/ex02/header/Array.hpp
@@ -0,0 +1,69 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+#include <cstddef>
+#include <stdexcept>
+
+template <typename T>
+class Array {
+private:
+	unsigned int _size;
+	T 			*_array;
+public:
+	Array<T>(): _size(0),  _array(NULL){ };
+	~Array<T>(){ delete [] _array; };
+	explicit Array<T>(unsigned int n): _size(n) , _array(NULL) {
+		if (_size != 0) {
+			this->_array = new T[n];
+		}
+	 };
+	Array<T>(const Array<T> &src): _size(src._size), _array(NULL) {
+		if (_size != 0) {
+			_array = new T[_size];
+		}
+		for (unsigned int i = 0; i < _size; i++) {
+			_array[i] = src._array[i];
+		}
+	};
+	Array &operator=(const Array &rhs) {
+		if (this != &rhs) {
+			delete [] _array;
+			_size = rhs.size();
+			_array = new T[_size];
+			for (unsigned int i = 0; i < _size; i++) {
+				_array[i] = rhs._array[i];
+			}
+		}
+		return *this;
+	};
+
+	T &operator[](unsigned int i) {
+		if (i < 0 || i >= _size) {
+			throw std::out_of_range("Array index out of range!\n");
+		}
+		return _array[i];
+	};
+
+	const T &operator[](unsigned int i) const {
+		if (i < 0 || i >= _size) {
+			throw std::out_of_range("Array index out of range!\n");
+		}
+		return _array[i];
+	};
+
+	unsigned int size() const { return _size; };
+
+};
+
+template<typename T>
+std::ostream &operator<<(std::ostream &o, Array<T> const &rhs) {
+	o << "[ ";
+	for (unsigned int i = 0; i < rhs.size(); i++) {
+		o << rhs[i];
+		if (i + 1 < rhs.size())
+			std::cout << ", ";
+	}
+	o << " ]" << std::endl;
+	return o;
+}
+
+#endif
